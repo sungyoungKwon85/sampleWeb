aae04b37628e1fafb62694cc31ca644291fc8e75
sungyoungKwon84 <ssyang0@naver.com>
2020-04-01 14:48:17 +0900

    real1, real2 무중단 배포 셋팅

---
+++ scripts/stop.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+
+echo "> $IDLE_PORT 에서 구동중인 어플 pid.."
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID} ]
+then
+  echo "> no running"
+else
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
+fi
